a6e11c358bd6451008bf67d926dc61214d08b04f
canario <germanfagalde@gmail.com>
2016-03-17 00:45:07 -0300

    fix migration eror

--- db/migrate/20160315032620_change_accessory_new_price_column_type.rb
+++ db/migrate/20160315032620_change_accessory_new_price_column_type.rb
@@ -1,5 +1,5 @@
 class ChangeAccessoryNewPriceColumnType < ActiveRecord::Migration
   def change
-    change_column :accessories, :new_price, 'integer USING CAST(new_price AS integer)'
+    change_column :accessories, :new_price, :integer
   end
 end
