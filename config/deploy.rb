547004acc2bedd0c2d2756a21287e9a1fc1bffb1
canario <germanfagalde@gmail.com>
2016-03-18 19:53:37 -0300

    change  deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,7 +69,9 @@ namespace :deploy do
 
   desc "build missing paperclip styles"
   task :build_missing_paperclip_styles do
-    run "cd #{current_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+    on roles(:app) do
+      execute "cd #{current_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+    end
   end
 
   desc 'Restart application'
