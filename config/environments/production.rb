37f597c90714257916d285d1619ab7004bc0514f
canario <germanfagalde@gmail.com>
2016-03-30 12:16:13 -0300

    add secret configuration in application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
     :domain               => 'mayacom.com.uy',
     :authentication       => :plain,
     :enable_starttls_auto => :true,
-    :user_name            => 'correcam1515@gmail.com',
-    :password             => 'Germ@n15'
+    :user_name            => Rails.application.secrets.gmail_username,
+    :password             => Rails.application.secrets.gmail_password
   }
 
 end
