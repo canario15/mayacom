42da78ee2d0d24128018846a8b276c130761ce60
Daniel Fernández <dfernandez@codigodelsur.com>
2016-04-04 23:57:22 -0300

    add capistrano 500.html task and change nginx config

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,6 +76,14 @@ namespace :deploy do
     end
   end
 
+  task :generate_500_html do
+    on roles(:web) do |host|
+      public_500_html = File.join(release_path, "public/500.html")
+      execute :curl, "-k", "https://#{host.hostname}/500", "> #{public_500_html}"
+    end
+  end
+  after "deploy:published", :generate_500_html
+
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
